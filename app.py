2883b5c1896109f59a0dc3c879f0dbf0eaceb84f
Emad DARWICH <108935365+emaddar@users.noreply.github.com>
2023-11-23 14:26:04 +0100

    to_excel

--- app.py
+++ app.py
@@ -70,22 +70,43 @@ def main():
                     mime='text/csv'
                 )
 
-                output = BytesIO()
+                # output = BytesIO()
 
-                # Write files to in-memory strings using BytesIO
-                # See: https://xlsxwriter.readthedocs.io/workbook.html?highlight=BytesIO#constructor
-                workbook = xlsxwriter.Workbook(output, {'in_memory': True})
-                worksheet = workbook.add_worksheet()
+                # # Write files to in-memory strings using BytesIO
+                # # See: https://xlsxwriter.readthedocs.io/workbook.html?highlight=BytesIO#constructor
+                # workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+                # worksheet = workbook.add_worksheet()
 
-                worksheet.write(df)
-                workbook.close()
+                # worksheet.write('A1', 'Hello')
+                # workbook.close()
 
-                st.download_button(
-                    label="Download Excel workbook",
-                    data=output.getvalue(),
-                    file_name="workbook.xlsx",
-                    mime="application/vnd.ms-excel"
-                )
+                # st.download_button(
+                #     label="Download Excel workbook",
+                #     data=output.getvalue(),
+                #     file_name="workbook.xlsx",
+                #     mime="application/vnd.ms-excel"
+                # )
+
+
+                from io import BytesIO
+                from pyxlsb import open_workbook as open_xlsb
+                import streamlit as st
+
+                def to_excel(df):
+                    output = BytesIO()
+                    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+                    df.to_excel(writer, index=False, sheet_name='Sheet1')
+                    workbook = writer.book
+                    worksheet = writer.sheets['Sheet1']
+                    format1 = workbook.add_format({'num_format': '0.00'}) 
+                    worksheet.set_column('A:A', None, format1)  
+                    writer.save()
+                    processed_data = output.getvalue()
+                    return processed_data
+                df_xlsx = to_excel(df)
+                st.download_button(label='📥 Download Current Result',
+                                                data=df_xlsx ,
+                                                file_name= 'df_test.xlsx')
                 
 
 if __name__ == "__main__":
