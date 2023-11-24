3508269f04949260e483cedb51031f4bd8ec74b9
Emad DARWICH <108935365+emaddar@users.noreply.github.com>
2023-11-24 09:24:35 +0100

    s

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if uploaded_file is not None:
     try:
         # Sélectionnez uniquement les colonnes 'Description', 'sous ensemble'
         selected_columns = ['Description', 'sous ensemble ']
-        df = df[selected_columns].head(50)
+        df = df[selected_columns]
     except:
         st.warning("Vérifiez si votre fichier Excel contient les deux colonnes : 'Description' et 'sous ensemble'.", icon="⚠️")
         
